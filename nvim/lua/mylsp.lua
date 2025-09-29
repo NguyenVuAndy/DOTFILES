24c051002cd2b01321966e351bba627d726dbfb0
Nguyen Vu An Dien <dien.nguyenvuan@gmail.com>
2025-09-29 07:47:31 +0700

    Fix lsp for C

--- nvim/lua/mylsp.lua
+++ nvim/lua/mylsp.lua
@@ -1,50 +1,48 @@
--- Mappings.
--- See `:help vim.diagnostic.*` for documentation on any of the below functions
-local opts = { noremap=true, silent=true }
-vim.api.nvim_set_keymap('n', ',e', '<cmd>lua vim.diagnostic.open_float()<CR>', opts)
-vim.api.nvim_set_keymap('n', 'ge', '<cmd>lua vim.diagnostic.goto_prev()<CR>', opts)
-vim.api.nvim_set_keymap('n', 'gE', '<cmd>lua vim.diagnostic.goto_next()<CR>', opts)
-vim.api.nvim_set_keymap('n', ',q', '<cmd>lua vim.diagnostic.setloclist()<CR>', opts)
-
--- Use an on_attach function to only map the following keys
 -- after the language server attaches to the current buffer
 local on_attach = function(client, bufnr)
+  local opts = { noremap=true, silent=true, buffer=bufnr }
+
   -- Enable completion triggered by <c-x><c-o>
-  vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+  vim.bo[bufnr].omnifunc = "v:lua.vim.lsp.omnifunc"
 
-  -- Mappings.
-  -- See `:help vim.lsp.*` for documentation on any of the below functions
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', 's', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'i', ',s', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', ',wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', ',wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', ',wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', ',D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', ',rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', ',qf', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', ',f', '<cmd>lua vim.lsp.buf.formatting()<CR>', opts)
+  -- Keymaps
+  vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+  vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+  vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+  vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+  vim.keymap.set('n', 's', vim.lsp.buf.signature_help, opts)
+  vim.keymap.set('i', ',s', vim.lsp.buf.signature_help, opts)
+  vim.keymap.set('n', ',wa', vim.lsp.buf.add_workspace_folder, opts)
+  vim.keymap.set('n', ',wr', vim.lsp.buf.remove_workspace_folder, opts)
+  vim.keymap.set('n', ',wl', function()
+    print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+  end, opts)
+  vim.keymap.set('n', ',D', vim.lsp.buf.type_definition, opts)
+  vim.keymap.set('n', ',rn', vim.lsp.buf.rename, opts)
+  vim.keymap.set('n', ',qf', vim.lsp.buf.code_action, opts)
+  vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+  vim.keymap.set('n', ',f', function()
+    vim.lsp.buf.format { async = true }
+  end, opts)
 end
 
--- Use a loop to conveniently call 'setup' on multiple servers and
--- map buffer local keybindings when the language server attaches
-local servers = { 'clangd', 'zls', 'pyright' }
-for _, lsp in pairs(servers) do
-  require('lspconfig')[lsp].setup {
-    on_attach = on_attach,
-    flags = {
-      -- This will be the default in neovim 0.7+
-      debounce_text_changes = 150,
-    }
-  }
-end
+-- global diagnostic settings
+vim.diagnostic.config({
+  virtual_text = true,  -- inline errors
+  signs = true,         -- gutter symbols
+  underline = true,     -- underline problem text
+  update_in_insert = false, -- don’t spam while typing
+  severity_sort = true,
+})
 
-require('lspconfig').clangd.setup{
+
+-- capabilities
+local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+-- clangd config
+vim.lsp.config("clangd", {
   on_attach = on_attach,
+  capabilities = capabilities,
   cmd = {
     "clangd",
     "--background-index",
@@ -58,18 +56,22 @@ require('lspconfig').clangd.setup{
     "--inlay-hints",
     "--header-insertion-decorators",
   },
-  filetypes = {"c", "cpp", "objc", "objcpp"},
-  -- root_dir = utils.root_pattern("compile_commands.json", "compile_flags.txt", ".git")
-  init_option = { fallbackFlags = {  "-std=c++2a"  } }
-}
+  filetypes = { "c", "cpp", "objc", "objcpp" },
+  init_option = { fallbackFlags = { "-std=c++2a" } },
+})
 
-local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
-require('lspconfig').zls.setup {
+-- zls config
+vim.lsp.config("zls", {
   on_attach = on_attach,
-  capabilities = capabilities
-}
+  capabilities = capabilities,
+})
 
-require('lspconfig').pyright.setup {
+-- pyright config
+vim.lsp.config("pyright", {
   on_attach = on_attach,
-  capabilities = capabilities
-}
+  capabilities = capabilities,
+})
+
+-- enable all
+vim.lsp.enable({ "clangd", "zls", "pyright" })
+
